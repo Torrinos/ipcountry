bf7c9de545d28ab77f54fc5ee7e9b0c9bb0e9c9e
Torrinos <ole.bjorne@gmail.com>
2016-06-11 01:31:57 -0500

    Initial Commit

---
+++ ipcountry/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  ipcountry
+//
+//  Created by Oleg Medvedev on 2016-06-11.
+//  Copyright © 2016 Oleg Medvedev. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
